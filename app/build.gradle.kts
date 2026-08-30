c30f4c196882b0d22c33da2d9684bd41546832b3
zwz211123 <139982393+zwz211123@users.noreply.github.com>
2026-08-30 19:35:45 +0800

    Bump app version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.zwz.pipedreamadblocker"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
